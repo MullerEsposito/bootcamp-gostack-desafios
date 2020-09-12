eb679532b4817a2532e099bee0163baa3beced09
Muller Esposito <mulleresposito@hotmail.com>
2020-09-12 02:33:59 -0300

    DATABASE-UPLOAD TEMPLATE INCLUDED

--- desafio-fundamentos-nodejs/src/services/CreateTransactionService.ts
+++ desafio-fundamentos-nodejs/src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error("Operation type isn't supported");
+    }
+
     const balance = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && balance.total < value) {
