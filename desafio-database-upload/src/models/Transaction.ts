4f8d245bdf03f448a8abee51f231eb3d1eae22ce
Muller Esposito <mulleresposito@hotmail.com>
2020-09-15 20:19:46 -0300

    TRANSACTION MODEL MAPPED TO ORM

--- desafio-database-upload/src/models/Transaction.ts
+++ desafio-database-upload/src/models/Transaction.ts
@@ -1,16 +1,31 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Category from "./Category";
+
+@Entity('transaction')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
-
+  
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
