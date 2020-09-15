881ce26f816bd2e3ae285d2cd079865ba401ea67
Muller Esposito <mulleresposito@hotmail.com>
2020-09-15 20:32:06 -0300

    MIGRATIONS CreateTableCategory AND CreateTableTransactions CREATED

---
+++ desafio-database-upload/src/database/migrations/1599895881325-CreateTableTransactions.ts
@@ -0,0 +1,65 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateTableTransactions1599891074129 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.createTable(new Table({
+            name: 'transactions',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'title',
+                    type: 'varchar',
+                },
+                {
+                    name: 'value',
+                    type: 'int',
+                },
+                {
+                    name: 'type',
+                    type: 'varchar',
+                },
+                {
+                    name: 'category_id',
+                    type: 'varchar',
+                    
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                }
+            ]
+        }))
+
+        await queryRunner.createForeignKey(
+            'transactions', 
+            new TableForeignKey({
+                name: 'TransactionCategory',
+                columnNames: ['category_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'category',
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropForeignKey('transactions', 'category_id_fk');
+        
+        await queryRunner.dropTable('transactions');
+    }
+
+}
