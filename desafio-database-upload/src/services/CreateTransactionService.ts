23cb70acae1eed0d27aab678912981ad57789263
Muller Esposito <mulleresposito@hotmail.com>
2020-09-15 20:25:10 -0300

    LOGIC TO SAVE TRANSACTION CREATED

--- desafio-database-upload/src/services/CreateTransactionService.ts
+++ desafio-database-upload/src/services/CreateTransactionService.ts
@@ -1,10 +1,28 @@
+import { getRepository } from "typeorm";
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({title, value, type, category}: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category,
+    })
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
